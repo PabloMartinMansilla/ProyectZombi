d523c1b245972bca9f47b9dd02982a458bb4161a
PabloMartinMansilla <pablomansilla4222@gmail.com>
2024-02-23 14:25:03 -0300

    Drawer and Big Drawer

--- Assets/scripts/Interface Player/Door.cs
+++ Assets/scripts/Interface Player/Door.cs
@@ -10,10 +10,6 @@ public class Door : MonoBehaviour, Interactable
 
     private void Start()
     {
-        BoxCollider colisionBox = gameObject.AddComponent<BoxCollider>();
-        colisionBox.size = new Vector3(1, 2.098418f, 4);
-        colisionBox.center = new Vector3(0, -0.9f, 0);
-        colisionBox.isTrigger = true;
         _animator = GetComponent<Animator>();
     }
 
