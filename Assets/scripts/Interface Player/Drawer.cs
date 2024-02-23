84062f48bf6d0f52ec272266cd5c8026f174baf3
PabloMartinMansilla <pablomansilla4222@gmail.com>
2024-02-23 02:13:54 -0300

    Interface Drawer and Door
    drawer and door animations with their scripts

--- Assets/scripts/Interface Player/Drawer.cs
+++ Assets/scripts/Interface Player/Drawer.cs
@@ -14,10 +14,6 @@ public class Drawer : MonoBehaviour, Interactable
 
     private void Start()
     {
-        BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
-        boxCollider.size = new Vector3(0.3375741f, 0.294036f, 0.4404921f);
-        boxCollider.center = new Vector3(0f, 0f, -0.27f);
-        boxCollider.isTrigger = true;
         _anim = gameObject.GetComponent<Animator>();
     }
 }
